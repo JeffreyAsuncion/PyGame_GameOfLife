e25d71f9c2c699005d976129feafe333f1079b91
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-20 13:55:32 -0500

    comments

--- PyGame_GameOfLife/PyGame_GameOfLife.py
+++ PyGame_GameOfLife/PyGame_GameOfLife.py
@@ -6,12 +6,12 @@ DEAD_COLOR = 0, 0, 0
 ALIVE_COLOR = 0, 255, 255
 
 pygame.init()
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(BOARD_SIZE)
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
+        if event.type == pygame.QUIT: 
+            sys.exit()
     screen.fill(DEAD_COLOR)
 
 
