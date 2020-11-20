b4be7fd880a527eaada38fe859958f032cdae390
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-11-20 13:47:51 -0500

    comments

---
+++ PyGame_GameOfLife/PyGame_GameOfLife.py
@@ -0,0 +1,19 @@
+import sys
+import pygame 
+
+BOARD_SIZE = WIDTH, HEIGHT = 640, 480
+DEAD_COLOR = 0, 0, 0
+ALIVE_COLOR = 0, 255, 255
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    screen.fill(DEAD_COLOR)
+
+
+    #screen.blit()
+    pygame.display.flip()
